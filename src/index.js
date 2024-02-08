6c4a003e5fc7dc361573e0c8868099bc23f807ed
binson thangjam <binsonsingh@gmail.com>
2024-02-08 13:25:48 +0530

    completed design

--- src/index.js
+++ src/index.js
@@ -1,29 +1,76 @@
 import "./style.css";
+import Clouds from "./assets/Clouds.jpg";
+import Snow from "./assets/Snow.jpg";
+import Rain from "./assets/Rain.jpg";
+import Clear from "./assets/Clear.jpg";
+import Thunderstorm from "./assets/Thunderstorm.jpg";
+import Default from "./assets/default.jpg";
 
 const location = document.getElementById("place");
 const tempDiv = document.getElementById("temperature");
 const condition = document.getElementById("condition");
 const weatherImage = document.createElement("img");
-const leftDiv = document.getElementById("left");
-leftDiv.appendChild(weatherImage);
+const weatherCondition = document.getElementById("weatherCondition");
+weatherCondition.prepend(weatherImage);
+const { body } = document;
+const content = document.getElementById("content");
 
-// tempDiv.appendChild(temp)
 const button = document.getElementById("location-button");
+let place;
 
+const bgImage = (data) => {
+  let imageUrl;
+  if (data) {
+    const weather = data.weather[0].main;
+    switch (weather) {
+      case "Clouds":
+        imageUrl = `url(${Clouds})`;
+        break;
+      case "Snow":
+        imageUrl = `url(${Snow})`;
+        break;
+      case "Rain":
+        imageUrl = `url(${Rain})`;
+        break;
+      case "Drizzle":
+        imageUrl = `url(${Rain})`;
+        break;
+      case "Clear":
+        imageUrl = `url(${Clear})`;
+        break;
+      case "Thunderstorm":
+        imageUrl = `url(${Thunderstorm})`;
+        break;
+      default:
+        imageUrl = `url(${Default})`; // Default to empty string if weather condition doesn't match
+    }
+  } else {
+    imageUrl = `url(${Default})`;
+  }
+  body.style.backgroundImage = imageUrl;
+};
+
+function toTitleCase(str) {
+  return str
+    .toLowerCase()
+    .replace(/(?:^|\s)\w/g, (match) => match.toUpperCase());
+}
 async function getWeather() {
-  const place = document.getElementById("location-input").value;
+  place = document.getElementById("location-input").value;
   const response = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=611c459610fd2350acf5d49c2372124b&units=metric`
   );
   const data = await response.json();
   const imgSrc = data.weather[0].icon;
-  location.innerText = place;
+  location.innerText = toTitleCase(place);
   tempDiv.innerText = `${data.main.temp}°C`;
-  condition.innerText = data.weather[0].description;
+  condition.innerText = toTitleCase(data.weather[0].description);
   weatherImage.src = `https://openweathermap.org/img/wn/${imgSrc}@2x.png`;
+  bgImage(data);
+  content.style.backgroundColor = "rgba(255, 255, 255, 0.1)";
 }
 
-async function getAir(city) {
+async function getAir() {
   const response = await fetch(
     `http://api.openweathermap.org/data/2.5/air_pollution?lat=50&lon=50&appid=611c459610fd2350acf5d49c2372124b`
   );
@@ -31,8 +78,5 @@ async function getAir(city) {
   console.log(data);
 }
 
-button.addEventListener("click", () => {
-  getWeather();
-});
-
-// getAir()
+button.addEventListener("click", getWeather);
+bgImage();
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,23 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-//   mode: 'production',
-  entry: './src/index.js',
+  //   mode: 'production',
+  entry: "./src/index.js",
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
 
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+
+        type: "asset/resource",
       },
     ],
   },
